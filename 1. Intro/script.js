b2c133e6f100eac208c29062098e88cdd3a935f0
Nazokat Saidikromova <nazokatsaidovna04@gmail.com>
2023-12-09 17:03:52 +0500

    intro to javascript, tutorials

---
+++ 1. Intro/script.js
@@ -0,0 +1,25 @@
+//  intro to js 
+//=======
+//was invented by Brendan Each
+//Developed for Netscape 2, livescript
+// became ECMA in 1997
+//========
+//What makes javascript unique?
+//full integration with HTML / CSS
+//Simple things are done simply
+//Supported by all major browsers and enabled by default
+//=========
+//compiling
+//1. in HTML (inspect, console)
+//2. with online compliers
+//3. in terminal (node js)
+//========
+
+
+
+
+
+
+
+
+
