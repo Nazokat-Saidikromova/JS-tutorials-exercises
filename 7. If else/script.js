24616528b97f130fe715ffdff5cf706239e7a2e8
Nazokat Saidikromova <nazokatsaidovna04@gmail.com>
2023-12-09 17:13:19 +0500

    exercises

---
+++ 7. If else/script.js
@@ -0,0 +1,15 @@
+//=========
+let title = "webbrain academy";
+if (title = "webbrain Academy")
+    console.log (true);
+else 
+console.log(false); // false because of "A"
+//==========
+if (title = "0")
+    console.log(true);
+else 
+console.log(false); // true because of string
+//===========
+let title = "0";
+if (title) console.log(true);
+else console.log (false) // true
