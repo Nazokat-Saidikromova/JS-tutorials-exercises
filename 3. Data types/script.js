b51ea8977b24313373147df019a534a788f996ba
Nazokat Saidikromova <nazokatsaidovna04@gmail.com>
2023-12-09 17:07:22 +0500

    Data types tutorial, exercises

---
+++ 3. Data types/script.js
@@ -0,0 +1,25 @@
+//Data types
+// homework
+let name = "Ilya";
+console.log(`hello ${1}`); // hello 1
+console.log(`hello ${"name"}`); // name
+console.log(`hello ${name}`); // hello Ilya
+
+//about data types
+// There are 8 data types in js
+// 7 primitive
+// 1 non- primitive
+
+// primitive data types
+// 1. number = > 9007199254740992
+// 2. BigInt => 9007199254740992n
+// 3. String => ""; ''; ``
+// 4. boolean => true / false
+// 5. undefined => key word bor, nomi bor, lekin ma`lumot biriktirilmagan
+// 6. null => key word bor, nomi bor, ma`lumot biriktirilgan lekin yo`q qiymat
+// 7. symbol=> unique id
+
+//non-primitive data types
+// object => array[], object {}, function ... (){}
+
+// typeof 
