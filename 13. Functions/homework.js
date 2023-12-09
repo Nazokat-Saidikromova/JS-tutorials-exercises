fc15ec4624b86664d5eade9168a2bc0850caf415
Nazokat Saidikromova <nazokatsaidovna04@gmail.com>
2023-12-09 17:23:01 +0500

    Functions tutorials and exercises

---
+++ 13. Functions/homework.js
@@ -0,0 +1,9 @@
+// homework 
+//N1
+function maxNumber (a , b) {
+    if (a === undefined || b === undefined){
+    return ("no info"); 
+    }
+    return Math.max (a, b);
+}
+console.log(maxNumber());
---
+++ 13. Functions/script.js
@@ -0,0 +1,19 @@
+//functions 
+//hoisting
+// console.log(a);
+// let a=12;
+//=======
+//syntaxys
+// function sayhi(){
+//     console.log ("hello")
+// }
+// sayhi()
+//=======
+//shadow variable
+
+let name = "Lily";
+let age = "20";
+// default parametr
+
+
+
