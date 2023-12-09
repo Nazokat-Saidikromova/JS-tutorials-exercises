ab89acda3e66846f2c38570d7bccb4dd65ef1b23
Nazokat Saidikromova <nazokatsaidovna04@gmail.com>
2023-12-09 17:06:01 +0500

    About variables, tutorial

---
+++ 2. Variables/script.js
@@ -0,0 +1,15 @@
+//Variables 
+//Named storage
+// single thread => bir vaqtning o`zida faqat bitta amal bajara olish
+//==========
+//variable decloration
+//Steps
+//1. key word (let, var, const)
+//2. o`zgaruvchining nomi
+//3. tenglik belgisi (=)
+//4. biriktiriladigan ma`lumot
+//5. e'lon qilish (console)
+//===========
+//const = > O`zgarmas. Yangi qiymat bera olmaymiz.
+// var = > global. Butun boshli proyektga ta`sir qiladi.
+// let = > scop. 
